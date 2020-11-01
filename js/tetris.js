2d86e3d7aa3503ed8b929d30530fa51985f93018
yondraco <hth.it.mm@gmail.com>
2020-11-02 03:35:25 +0700

    first commit

---
+++ js/tetris.js
@@ -0,0 +1,372 @@
+// Khai báo các biến const tetromino là các mảng chứa các mảnh tetris
+const I = [
+    [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+    ]
+];
+
+const J = [
+    [
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 1]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+    ]
+];
+
+const L = [
+    [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0]
+    ],
+    [
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const O = [
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+    ]
+];
+
+const S = [
+    [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1]
+    ],
+    [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0]
+    ],
+    [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const T = [
+    [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const Z = [
+    [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 0, 1],
+        [0, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 0],
+        [1, 0, 0]
+    ]
+];
+
+// Vẽ trên canvas
+const cvs = document.getElementById("tetris");
+const ctx = cvs.getContext("2d");
+const scoreElement = document.getElementById("score");
+
+const ROW = 20;
+const COLUMN = 10;
+const SS = 20; 
+const ES = "white"; 
+
+function drawSquare(x,y,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x*SS,y*SS,SS,SS);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x*SS,y*SS,SS,SS);
+}
+
+let board = [];
+for( r = 0; r <ROW; r++){
+    board[r] = [];
+    for(c = 0; c < COLUMN; c++){
+        board[r][c] = ES;
+    }
+}
+
+function drawBoard(){
+    for( r = 0; r <ROW; r++){
+        for(c = 0; c < COLUMN; c++){
+            drawSquare(c,r,board[r][c]);
+        }
+    }
+}
+drawBoard();
+
+const PIECES = [
+    [Z,"purple"],
+    [S,"purple"],
+    [T,"green"],
+    [O,"#FFFF44"],
+    [L,"#FF8800"],
+    [I,"#44FFFF"],
+    [J,"#FF8800"]
+];
+
+function randomPiece(){
+    let r = randomN = Math.floor(Math.random() * PIECES.length)
+    return new Piece( PIECES[r][0],PIECES[r][1]);
+}
+let p = randomPiece();
+
+function Piece(tetromino,color){
+    this.tetromino = tetromino;
+    this.color = color;
+    this.tetrominoN = 0; 
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+    // Điều khiển các mảnh
+    this.x = 3;
+    this.y = -2;
+}
+
+Piece.prototype.fill = function(color){
+    for( r = 0; r < this.activeTetromino.length; r++){
+        for(c = 0; c < this.activeTetromino.length; c++){
+            if( this.activeTetromino[r][c]){
+                drawSquare(this.x + c,this.y + r, color);
+            }
+        }
+    }
+}
+
+Piece.prototype.draw = function(){
+    this.fill(this.color);
+}
+
+Piece.prototype.unDraw = function(){
+    this.fill(ES);
+}
+
+Piece.prototype.moveDown = function(){
+    if(!this.collision(0,1,this.activeTetromino)){
+        this.unDraw();
+        this.y++;
+        this.draw();
+    }else{
+        this.lock();
+        p = randomPiece();
+    }
+}
+
+Piece.prototype.moveRight = function(){
+    if(!this.collision(1,0,this.activeTetromino)){
+        this.unDraw();
+        this.x++;
+        this.draw();
+    }
+}
+
+Piece.prototype.moveLeft = function(){
+    if(!this.collision(-1,0,this.activeTetromino)){
+        this.unDraw();
+        this.x--;
+        this.draw();
+    }
+}
+
+Piece.prototype.rotate = function(){
+    let nextPattern = this.tetromino[(this.tetrominoN + 1)%this.tetromino.length];
+    let kick = 0;
+    
+    if(this.collision(0,0,nextPattern)){
+        if(this.x > COLUMN/2){
+            kick = -1; 
+        }else{
+            kick = 1;
+        }
+    }
+    
+    if(!this.collision(kick,0,nextPattern)){
+        this.unDraw();
+        this.x += kick;
+        this.tetrominoN = (this.tetrominoN + 1)%this.tetromino.length;
+        this.activeTetromino = this.tetromino[this.tetrominoN];
+        this.draw();
+    }
+}
+
+let score = 0;
+
+Piece.prototype.lock = function(){
+    for( r = 0; r < this.activeTetromino.length; r++){
+        for(c = 0; c < this.activeTetromino.length; c++){
+            if( !this.activeTetromino[r][c]){
+                continue;
+            }
+            if(this.y + r < 0){
+                alert("Game Over");
+                gameOver = true;
+                break;
+            }
+            board[this.y+r][this.x+c] = this.color;
+        }
+    }
+    for(r = 0; r < ROW; r++){
+        let isRowFull = true;
+        for( c = 0; c < COLUMN; c++){
+            isRowFull = isRowFull && (board[r][c] != ES);
+        }
+        if(isRowFull){
+            for( y = r; y > 1; y--){
+                for( c = 0; c < COLUMN; c++){
+                    board[y][c] = board[y-1][c];
+                }
+            }
+            for( c = 0; c < COLUMN; c++){
+                board[0][c] = ES;
+            }
+            score += 10;
+        }
+    }
+    // Cập nhật board
+    drawBoard();
+    // Cập nhật điểm
+    scoreElement.innerHTML = score;
+}
+
+Piece.prototype.collision = function(x,y,piece){
+    for( r = 0; r < piece.length; r++){
+        for(c = 0; c < piece.length; c++){
+            if(!piece[r][c]){
+                continue;
+            }
+            let newX = this.x + c + x;
+            let newY = this.y + r + y;
+            
+            if(newX < 0 || newX >= COLUMN || newY >= ROW){
+                return true;
+            }
+            if(newY < 0){
+                continue;
+            }
+            if( board[newY][newX] != ES){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Điều khiển các mảnh
+document.addEventListener("keydown",CONTROL);
+
+function CONTROL(event){
+    if(event.keyCode == 37){
+        p.moveLeft();
+        dropStart = Date.now();
+    }else if(event.keyCode == 38){
+        p.rotate();
+        dropStart = Date.now();
+    }else if(event.keyCode == 39){
+        p.moveRight();
+        dropStart = Date.now();
+    }else if(event.keyCode == 40){
+        p.moveDown();
+    }
+}
+
+let dropStart = Date.now();
+let gameOver = false;
+function drop(){
+    let now = Date.now();
+    let delta = now - dropStart;
+    if(delta > 1000){
+        p.moveDown();
+        dropStart = Date.now();
+    }
+    if( !gameOver){
+        requestAnimationFrame(drop);
+    }
+}
+drop();
