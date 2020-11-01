ce8c3dcff6f65d737a0cda5970e618ada632f48e
yondraco <hth.it.mm@gmail.com>
2020-11-02 03:59:38 +0700

    first commit

--- js/tetris.js
+++ js/tetris.js
@@ -157,120 +157,120 @@ const scoreElement = document.getElementById("score");
 
 const ROW = 20;
 const COLUMN = 10;
-const SS = 20; 
-const ES = "white"; 
+const SS = 20;
+const ES = "white";
 
-function drawSquare(x,y,color){
+function drawSquare(x, y, color) {
     ctx.fillStyle = color;
-    ctx.fillRect(x*SS,y*SS,SS,SS);
+    ctx.fillRect(x * SS, y * SS, SS, SS);
     ctx.strokeStyle = "black";
-    ctx.strokeRect(x*SS,y*SS,SS,SS);
+    ctx.strokeRect(x * SS, y * SS, SS, SS);
 }
 
 let board = [];
-for( r = 0; r <ROW; r++){
+for (r = 0; r < ROW; r++) {
     board[r] = [];
-    for(c = 0; c < COLUMN; c++){
+    for (c = 0; c < COLUMN; c++) {
         board[r][c] = ES;
     }
 }
 
-function drawBoard(){
-    for( r = 0; r <ROW; r++){
-        for(c = 0; c < COLUMN; c++){
-            drawSquare(c,r,board[r][c]);
+function drawBoard() {
+    for (r = 0; r < ROW; r++) {
+        for (c = 0; c < COLUMN; c++) {
+            drawSquare(c, r, board[r][c]);
         }
     }
 }
 drawBoard();
 
 const PIECES = [
-    [Z,"purple"],
-    [S,"purple"],
-    [T,"green"],
-    [O,"#FFFF44"],
-    [L,"#FF8800"],
-    [I,"#44FFFF"],
-    [J,"#FF8800"]
+    [Z, "purple"],
+    [S, "purple"],
+    [T, "green"],
+    [O, "#FFFF44"],
+    [L, "#FF8800"],
+    [I, "#44FFFF"],
+    [J, "#FF8800"]
 ];
 
-function randomPiece(){
+function randomPiece() {
     let r = randomN = Math.floor(Math.random() * PIECES.length)
-    return new Piece( PIECES[r][0],PIECES[r][1]);
+    return new Piece(PIECES[r][0], PIECES[r][1]);
 }
 let p = randomPiece();
 
-function Piece(tetromino,color){
+function Piece(tetromino, color) {
     this.tetromino = tetromino;
     this.color = color;
-    this.tetrominoN = 0; 
+    this.tetrominoN = 0;
     this.activeTetromino = this.tetromino[this.tetrominoN];
     // Điều khiển các mảnh
     this.x = 3;
     this.y = -2;
 }
 
-Piece.prototype.fill = function(color){
-    for( r = 0; r < this.activeTetromino.length; r++){
-        for(c = 0; c < this.activeTetromino.length; c++){
-            if( this.activeTetromino[r][c]){
-                drawSquare(this.x + c,this.y + r, color);
+Piece.prototype.fill = function (color) {
+    for (r = 0; r < this.activeTetromino.length; r++) {
+        for (c = 0; c < this.activeTetromino.length; c++) {
+            if (this.activeTetromino[r][c]) {
+                drawSquare(this.x + c, this.y + r, color);
             }
         }
     }
 }
 
-Piece.prototype.draw = function(){
+Piece.prototype.draw = function () {
     this.fill(this.color);
 }
 
-Piece.prototype.unDraw = function(){
+Piece.prototype.unDraw = function () {
     this.fill(ES);
 }
 
-Piece.prototype.moveDown = function(){
-    if(!this.collision(0,1,this.activeTetromino)){
+Piece.prototype.moveDown = function () {
+    if (!this.collision(0, 1, this.activeTetromino)) {
         this.unDraw();
         this.y++;
         this.draw();
-    }else{
+    } else {
         this.lock();
         p = randomPiece();
     }
 }
 
-Piece.prototype.moveRight = function(){
-    if(!this.collision(1,0,this.activeTetromino)){
+Piece.prototype.moveRight = function () {
+    if (!this.collision(1, 0, this.activeTetromino)) {
         this.unDraw();
         this.x++;
         this.draw();
     }
 }
 
-Piece.prototype.moveLeft = function(){
-    if(!this.collision(-1,0,this.activeTetromino)){
+Piece.prototype.moveLeft = function () {
+    if (!this.collision(-1, 0, this.activeTetromino)) {
         this.unDraw();
         this.x--;
         this.draw();
     }
 }
 
-Piece.prototype.rotate = function(){
-    let nextPattern = this.tetromino[(this.tetrominoN + 1)%this.tetromino.length];
+Piece.prototype.rotate = function () {
+    let nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
     let kick = 0;
-    
-    if(this.collision(0,0,nextPattern)){
-        if(this.x > COLUMN/2){
-            kick = -1; 
-        }else{
+
+    if (this.collision(0, 0, nextPattern)) {
+        if (this.x > COLUMN / 2) {
+            kick = -1;
+        } else {
             kick = 1;
         }
     }
-    
-    if(!this.collision(kick,0,nextPattern)){
+
+    if (!this.collision(kick, 0, nextPattern)) {
         this.unDraw();
         this.x += kick;
-        this.tetrominoN = (this.tetrominoN + 1)%this.tetromino.length;
+        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
         this.activeTetromino = this.tetromino[this.tetrominoN];
         this.draw();
     }
@@ -278,32 +278,32 @@ Piece.prototype.rotate = function(){
 
 let score = 0;
 
-Piece.prototype.lock = function(){
-    for( r = 0; r < this.activeTetromino.length; r++){
-        for(c = 0; c < this.activeTetromino.length; c++){
-            if( !this.activeTetromino[r][c]){
+Piece.prototype.lock = function () {
+    for (r = 0; r < this.activeTetromino.length; r++) {
+        for (c = 0; c < this.activeTetromino.length; c++) {
+            if (!this.activeTetromino[r][c]) {
                 continue;
             }
-            if(this.y + r < 0){
+            if (this.y + r < 0) {
                 alert("Game Over");
                 gameOver = true;
                 break;
             }
-            board[this.y+r][this.x+c] = this.color;
+            board[this.y + r][this.x + c] = this.color;
         }
     }
-    for(r = 0; r < ROW; r++){
+    for (r = 0; r < ROW; r++) {
         let isRowFull = true;
-        for( c = 0; c < COLUMN; c++){
+        for (c = 0; c < COLUMN; c++) {
             isRowFull = isRowFull && (board[r][c] != ES);
         }
-        if(isRowFull){
-            for( y = r; y > 1; y--){
-                for( c = 0; c < COLUMN; c++){
-                    board[y][c] = board[y-1][c];
+        if (isRowFull) {
+            for (y = r; y > 1; y--) {
+                for (c = 0; c < COLUMN; c++) {
+                    board[y][c] = board[y - 1][c];
                 }
             }
-            for( c = 0; c < COLUMN; c++){
+            for (c = 0; c < COLUMN; c++) {
                 board[0][c] = ES;
             }
             score += 10;
@@ -315,22 +315,22 @@ Piece.prototype.lock = function(){
     scoreElement.innerHTML = score;
 }
 
-Piece.prototype.collision = function(x,y,piece){
-    for( r = 0; r < piece.length; r++){
-        for(c = 0; c < piece.length; c++){
-            if(!piece[r][c]){
+Piece.prototype.collision = function (x, y, piece) {
+    for (r = 0; r < piece.length; r++) {
+        for (c = 0; c < piece.length; c++) {
+            if (!piece[r][c]) {
                 continue;
             }
             let newX = this.x + c + x;
             let newY = this.y + r + y;
-            
-            if(newX < 0 || newX >= COLUMN || newY >= ROW){
+
+            if (newX < 0 || newX >= COLUMN || newY >= ROW) {
                 return true;
             }
-            if(newY < 0){
+            if (newY < 0) {
                 continue;
             }
-            if( board[newY][newX] != ES){
+            if (board[newY][newX] != ES) {
                 return true;
             }
         }
@@ -339,33 +339,33 @@ Piece.prototype.collision = function(x,y,piece){
 }
 
 // Điều khiển các mảnh
-document.addEventListener("keydown",CONTROL);
+document.addEventListener("keydown", CONTROL);
 
-function CONTROL(event){
-    if(event.keyCode == 37){
+function CONTROL(event) {
+    if (event.keyCode == 37) {
         p.moveLeft();
         dropStart = Date.now();
-    }else if(event.keyCode == 38){
+    } else if (event.keyCode == 38) {
         p.rotate();
         dropStart = Date.now();
-    }else if(event.keyCode == 39){
+    } else if (event.keyCode == 39) {
         p.moveRight();
         dropStart = Date.now();
-    }else if(event.keyCode == 40){
+    } else if (event.keyCode == 40) {
         p.moveDown();
     }
 }
 
 let dropStart = Date.now();
 let gameOver = false;
-function drop(){
+function drop() {
     let now = Date.now();
     let delta = now - dropStart;
-    if(delta > 1000){
+    if (delta > 1000) {
         p.moveDown();
         dropStart = Date.now();
     }
-    if( !gameOver){
+    if (!gameOver) {
         requestAnimationFrame(drop);
     }
 }
